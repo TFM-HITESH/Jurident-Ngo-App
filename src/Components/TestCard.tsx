a7ce4b7b52680d3b3a8cab5dc93e635e60a3a502
TFM-HITESH <hiteshshivkumar@gmail.com>
2023-12-27 23:42:32 +0530

    Test Commit

---
+++ src/Components/TestCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type TestCardProps = {
+    
+};
+
+const TestCard:React.FC<TestCardProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default TestCard;
---
+++ src/Pages/TestPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type TestPageProps = {
+    
+};
+
+const TestPage:React.FC<TestPageProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default TestPage;
